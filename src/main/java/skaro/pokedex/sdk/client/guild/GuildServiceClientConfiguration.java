f54d6930661167ea001c69b6983580e05044646f
unknown <bencbchurchill@gmail.com>
2021-07-04 17:33:17 -0700

    Removed incorrect logging filter

--- src/main/java/skaro/pokedex/sdk/client/guild/GuildServiceClientConfiguration.java
+++ src/main/java/skaro/pokedex/sdk/client/guild/GuildServiceClientConfiguration.java
@@ -1,24 +1,16 @@
 package skaro.pokedex.sdk.client.guild;
 
 import java.util.Optional;
-import java.util.function.Function;
-import java.util.function.Predicate;
 
 import javax.validation.Valid;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.web.reactive.function.client.ClientResponse;
-import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
-import org.springframework.web.reactive.function.client.ExchangeFilterFunctions;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import reactor.netty.http.client.HttpClient;
@@ -44,24 +36,12 @@ public class GuildServiceClientConfiguration {
 				.clientConnector(new ReactorClientHttpConnector(httpClient))
 				.baseUrl(configurationProperties.getBaseUri().toString())
 				.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-				.filter(createErrorStatusFilter())
 				.build();
 	}
 	
 	@Bean	
 	public GuildServiceClient guildServiceClient(@Qualifier(GUILD_SERVICE_WEBCLIENT_BEAN) WebClient webClient, Optional<CacheFacade> cacheFacade) {
 		return new CachingGuildServiceClient(webClient, cacheFacade);
-	}
-	
-	private ExchangeFilterFunction createErrorStatusFilter() {
-		Predicate<HttpStatus> clientErrorNot404 =  status -> status.is4xxClientError() && status != HttpStatus.NOT_FOUND;
-		Predicate<HttpStatus> serverError = status -> status.is5xxServerError();
-		Function<ClientResponse, ? extends Throwable> logError = response -> {
-			Logger log = LoggerFactory.getLogger(GuildServiceClient.class);
-			log.warn("Guild service returned a {}", response.statusCode());
-			return response.createException().block();
-		};
-		return ExchangeFilterFunctions.statusError(clientErrorNot404.or(serverError), logError);
 	}
 	
 }
